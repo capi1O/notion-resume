5e41b9bc148000413ef204c558065f986723423c
capi1O <18724661+capi1O@users.noreply.github.com>
2024-02-23 22:05:23 +0100

    fix exports

--- scripts/logging.js
+++ scripts/logging.js
@@ -1,7 +1,10 @@
-export const logGreen = (text) => {
+ const logGreen = (text) => {
 	console.log('\x1b[32m%s\x1b[0m', text);
 }
 
-export const logRed = (text) => {
+const logRed = (text) => {
 	console.log('\x1b[31m%s\x1b[0m', text);
 }
+
+exports.logGreen = logGreen;
+exports.logRed = logRed;
