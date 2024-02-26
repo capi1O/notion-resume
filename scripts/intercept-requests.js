cd180e5e317e4e2275585828c016090ed43d8c7c
capi1O <18724661+capi1O@users.noreply.github.com>
2024-02-26 14:56:08 +0100

    fix local file name on download

--- scripts/intercept-requests.js
+++ scripts/intercept-requests.js
@@ -1,4 +1,5 @@
 const download = require('download');
+const fs = require('fs').promises;
 const path = require('path');
 
 
@@ -16,16 +17,22 @@ const setupRequestInterception = async (page) => {
 				const extension = path.extname(url);
 				// const uid = uuidv4();
 				assetNumber++;
-				const localFilePath = `./notion-page/assets/${assetNumber}${extension}`;
+				const assetsDirectory = './notion-page/assets/';
+				const localFileName = `${assetNumber}${extension}`;
+				const originalFilePath = path.join(assetsDirectory, path.basename(url));
+				const localFilePath = path.join(assetsDirectory, localFileName);
 				console.log(`asset \x1b[34m${assetNumber}\x1b[0m requested, will download \x1b[4m${url}\x1b[0m to \x1b[33m${localFilePath}\x1b[0m`);
 				const _assetNumber = assetNumber; 
 				// prepare the promise but do not execute it
 				const downloadPromiseGenerator = () =>
-					download(url, localFilePath)
+					download(url, assetsDirectory)
 					.then((result) => {
 						console.log(`\x1b[32masset \x1b[34m#${_assetNumber}\x1b[32m downloaded\x1b[0m`);
 						downloadedAssetsObjects.push({ url, assetNumber: _assetNumber, extension });
 						// pendingDownloads--;
+
+						// rename file
+						return fs.rename(originalFilePath, localFilePath); // Rename the file
 					})
 					.catch((error) => {
 						console.log(`\x1b[31masset \x1b[34m#${_assetNumber}\x1b[31m download failed\x1b[0m`, error.message);
